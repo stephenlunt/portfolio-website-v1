b89d1b83e1463376a0781d6621c9428125ff588e
stephenlunt97 <stephenlunt@me.com>
2022-11-26 17:26:30 +0000

    feat: vercel analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
 
 import Layout from '../components/layout'
 import '../styles/globals.css'
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps, router }) {
     <Layout router={router}>
       <ThemeProvider enableSystem={true} attribute="class">
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </Layout>
   )
