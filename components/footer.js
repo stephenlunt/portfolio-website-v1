462414040ed4632fbcd89d0f15ee9f67fb28f9be
stephenlunt97 <stephenlunt@me.com>
2023-01-02 19:46:44 +0000

    chore(footer): update year

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
       id="footer"
       className="my-2 mx-auto flex translate-y-24 flex-col items-center justify-center py-2"
     >
-      <p>&copy; 2022 Stephen Lunt.</p>
+      <p>&copy; 2023 Stephen Lunt.</p>
     </div>
   )
 }
