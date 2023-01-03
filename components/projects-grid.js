31c4f59147d81bec0fc76b9b0e20086f7a5e01cf
stephenlunt97 <stephenlunt@me.com>
2023-01-03 11:19:03 +0000

    feat(projects): add ability to hide project tiles

--- components/projects-grid.js
+++ components/projects-grid.js
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-// TODO: implement featured and hidden projects
 const ProjectsGrid = ({ projects }) => {
   return (
     <section
@@ -11,24 +10,28 @@ const ProjectsGrid = ({ projects }) => {
       <h2 className="mx-auto flex justify-center p-4 text-2xl">Projects</h2>
 
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-        {projects.map((project, index) => (
-          <Link key={index} href={'/projects/' + project.slug}>
-            <div className="relative z-20 aspect-[3/2] cursor-pointer rounded-md bg-black/20 drop-shadow-xl">
-              <Image
-                src={project.imgUrl}
-                alt={project.imgAlt}
-                priority={true}
-                layout="fill"
-                className="rounded-md opacity-90 transition-opacity duration-500 hover:opacity-100"
-              />
-              <div className="absolute bottom-0 z-30 w-full rounded-b-md bg-black/30 py-1">
-                <h3 className="mx-auto flex flex-row justify-center text-white">
-                  {project.title}
-                </h3>
-              </div>
-            </div>
-          </Link>
-        ))}
+        {projects.map((project, index) => {
+          if (project.featured && project.visible) {
+            return (
+              <Link key={index} href={'/projects/' + project.slug}>
+                <div className="relative z-20 aspect-[3/2] cursor-pointer rounded-md bg-black/20 drop-shadow-xl">
+                  <Image
+                    src={project.imgUrl}
+                    alt={project.imgAlt}
+                    priority={true}
+                    layout="fill"
+                    className="rounded-md opacity-90 transition-opacity duration-500 hover:opacity-100"
+                  />
+                  <div className="absolute bottom-0 z-30 w-full rounded-b-md bg-black/30 py-1">
+                    <h3 className="mx-auto flex flex-row justify-center text-white">
+                      {project.title}
+                    </h3>
+                  </div>
+                </div>
+              </Link>
+            )
+          }
+        })}
       </div>
     </section>
   )
